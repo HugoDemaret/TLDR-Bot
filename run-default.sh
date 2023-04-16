19fed92fffb5ceed2eeb33acee8f19dfd8b27e75
Hugo Demaret <hugovladimir2000@gmail.com>
2023-04-16 23:39:18 +0200

    Update run-default.sh

--- run-default.sh
+++ run-default.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+cat << 'EOF'
+  _____ _           ____  ____
+ |_   _| |      _  |  _ \|  _ \
+   | | | |     (_) | | | | |_) |
+   | | | |___   _  | |_| |  _ <
+   |_| |_____| ( ) |____/|_| \_\
+               |/
+  Too  Long    ;   Didn't Read
+EOF
 
 # Run the bot using python3
 
